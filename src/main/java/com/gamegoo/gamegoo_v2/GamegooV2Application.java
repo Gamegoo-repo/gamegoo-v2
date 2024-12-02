f127f57e5976f3016d1c5491d96e89e1ef67b89a
Rimi <lora3226@inu.ac.kr>
2024-12-02 23:15:12 +0900

    :rocket: [Init] 프로젝트 생성

---
+++ src/main/java/com/gamegoo/gamegoo_v2/GamegooV2Application.java
@@ -0,0 +1,13 @@
+package com.gamegoo.gamegoo_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamegooV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamegooV2Application.class, args);
+	}
+
+}
