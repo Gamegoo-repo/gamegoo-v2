d844791887f34161f609c5d77af917caffa696e8
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-12-03 22:03:06 +0900

    :sparkles: feat: swagger 테스트용 API 추가

---
+++ src/main/java/com/gamegoo/gamegoo_v2/HomeController.java
@@ -0,0 +1,25 @@
+package com.gamegoo.gamegoo_v2;
+
+import com.gamegoo.gamegoo_v2.common.ApiResponse;
+import com.gamegoo.gamegoo_v2.exception.ErrorCode;
+import com.gamegoo.gamegoo_v2.exception.GlobalException;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Operation(summary = "홈 엔드포인트", description = "API 서비스 상태를 확인합니다.")
+    @GetMapping("/home")
+    public ApiResponse<String> home() {
+        return ApiResponse.ok("Gamegoo V2 API 서비스 입니다. 환영합니다.");
+    }
+
+    @Operation(summary = "에러 테스트", description = "예외를 발생시켜 테스트합니다.")
+    @GetMapping("/errortest")
+    public ApiResponse<Object> error() {
+        throw new GlobalException(ErrorCode._BAD_REQUEST);
+    }
+
+}
