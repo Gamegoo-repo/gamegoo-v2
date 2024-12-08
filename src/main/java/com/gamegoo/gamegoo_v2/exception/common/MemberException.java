e1c51b07d9f09c1404f56d9a401aa539ad4c4fb3
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-12-08 22:15:04 +0900

    :recycle: [Refactor] MemberException 파일 위치 변경

--- src/main/java/com/gamegoo/gamegoo_v2/exception/common/MemberException.java
+++
@@ -1,9 +0,0 @@
-package com.gamegoo.gamegoo_v2.exception.common;
-
-public class MemberException extends GlobalException {
-
-    public MemberException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-
-}
