d87d9dfa0d206589f5b3558d04698a07efb8ddcf
Rimi <lora3226@inu.ac.kr>
2024-12-20 12:18:09 +0900

    :sparkles:[Feat] 회원가입 - Riot Info 불러오기 구현

--- src/main/java/com/gamegoo/gamegoo_v2/auth/service/AuthFacadeService.java
+++ src/main/java/com/gamegoo/gamegoo_v2/auth/service/AuthFacadeService.java
@@ -2,6 +2,7 @@ package com.gamegoo.gamegoo_v2.auth.service;
 
 import com.gamegoo.gamegoo_v2.auth.dto.JoinRequest;
 import com.gamegoo.gamegoo_v2.member.service.MemberService;
+import com.gamegoo.gamegoo_v2.riot.dto.TierDetails;
 import com.gamegoo.gamegoo_v2.riot.service.RiotAccountService;
 import com.gamegoo.gamegoo_v2.riot.service.RiotInfoService;
 import com.gamegoo.gamegoo_v2.riot.service.RiotRecordService;
@@ -33,9 +34,14 @@ public class AuthFacadeService {
         // 2. [Riot] 존재하는 소환사인지 검증 & puuid 얻기
         String puuid = riotAccountService.getPuuid(request.getGameName(), request.getTag());
 
+        // 3. [Riot] summonerId 얻기
+        String summonerId = riotAccountService.getSummonerId(puuid);
+
         // 3. [Riot] tier, rank, winrate 얻기
+        TierDetails tierWinrateRank = riotInfoService.getTierWinrateRank(summonerId);
 
         // 4. [Member] member DB에 저장
+        
 
         // 5. [Riot] 최근 사용한 챔피언 3개 매핑
 
