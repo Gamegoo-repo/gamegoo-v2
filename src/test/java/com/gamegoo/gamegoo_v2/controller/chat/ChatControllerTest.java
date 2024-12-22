74f7986e6c0042052d7d1fb8ce2b3850b96f4410
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-12-23 00:46:42 +0900

    :white_check_mark: [Test] 특정 글을 통한 채팅방 시작 API 컨트롤러 테스트

--- src/test/java/com/gamegoo/gamegoo_v2/controller/chat/ChatControllerTest.java
+++ src/test/java/com/gamegoo/gamegoo_v2/controller/chat/ChatControllerTest.java
@@ -3,6 +3,7 @@ package com.gamegoo.gamegoo_v2.controller.chat;
 import com.gamegoo.gamegoo_v2.chat.controller.ChatController;
 import com.gamegoo.gamegoo_v2.chat.dto.response.ChatMessageListResponse;
 import com.gamegoo.gamegoo_v2.chat.dto.response.EnterChatroomResponse;
+import com.gamegoo.gamegoo_v2.chat.dto.response.SystemFlagResponse;
 import com.gamegoo.gamegoo_v2.chat.service.ChatFacadeService;
 import com.gamegoo.gamegoo_v2.controller.ControllerTestSupport;
 import com.gamegoo.gamegoo_v2.member.domain.Member;
@@ -29,6 +30,7 @@ class ChatControllerTest extends ControllerTestSupport {
 
     private static final String API_URL_PREFIX = "/api/v2";
     private static final Long TARGET_MEMBER_ID = 2L;
+    private static final Long TARGET_BOARD_ID = 1L;
     private static final String TARGET_CHATROOM_UUID = "test-uuid";
     private static final String TARGET_GAMENAME = "targetMember";
 
@@ -46,6 +48,7 @@ class ChatControllerTest extends ControllerTestSupport {
                     .hasNext(false)
                     .nextCursor(null)
                     .build();
+
             EnterChatroomResponse response = EnterChatroomResponse.builder()
                     .memberId(TARGET_MEMBER_ID)
                     .gameName(TARGET_GAMENAME)
@@ -75,6 +78,61 @@ class ChatControllerTest extends ControllerTestSupport {
                     .andExpect(jsonPath("$.data.friendRequestMemberId").isEmpty())
                     .andExpect(jsonPath("$.data.uuid").value(TARGET_CHATROOM_UUID))
                     .andExpect(jsonPath("$.data.system").isEmpty());
+        }
+
+    }
+
+    @Nested
+    @DisplayName("특정 글을 통한 채팅방 시작")
+    class StartChatroomByBoardIdTest {
+
+        @DisplayName("성공: 채팅방 및 상대 회원 정보, 시스템 메시지 정보, 대화 내역이 반환된다.")
+        @Test
+        void startChatroomByBoardIdSucceeds() throws Exception {
+            // given
+            ChatMessageListResponse chatMessageListResponse = ChatMessageListResponse.builder()
+                    .chatMessageList(new ArrayList<>())
+                    .listSize(0)
+                    .hasNext(false)
+                    .nextCursor(null)
+                    .build();
+
+            SystemFlagResponse systemFlagResponse = SystemFlagResponse.builder()
+                    .flag(1)
+                    .boardId(TARGET_BOARD_ID)
+                    .build();
+
+            EnterChatroomResponse response = EnterChatroomResponse.builder()
+                    .memberId(TARGET_MEMBER_ID)
+                    .gameName(TARGET_GAMENAME)
+                    .memberProfileImg(1)
+                    .blind(false)
+                    .blocked(false)
+                    .friend(false)
+                    .friendRequestMemberId(null)
+                    .uuid(TARGET_CHATROOM_UUID)
+                    .system(systemFlagResponse)
+                    .chatMessageListResponse(chatMessageListResponse)
+                    .build();
+
+            given(chatFacadeService.startChatroomByBoardId(any(Member.class), eq(TARGET_BOARD_ID))).willReturn(response);
+
+            // when // then
+            mockMvc.perform(get(API_URL_PREFIX + "/chat/start/board/{boardId}", TARGET_BOARD_ID))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.message").value("OK"))
+                    .andExpect(jsonPath("$.data.chatMessageListResponse").isNotEmpty())
+                    .andExpect(jsonPath("$.data.memberId").value(TARGET_MEMBER_ID))
+                    .andExpect(jsonPath("$.data.gameName").value(TARGET_GAMENAME))
+                    .andExpect(jsonPath("$.data.memberProfileImg").value(1))
+                    .andExpect(jsonPath("$.data.blind").value(false))
+                    .andExpect(jsonPath("$.data.blocked").value(false))
+                    .andExpect(jsonPath("$.data.friend").value(false))
+                    .andExpect(jsonPath("$.data.friendRequestMemberId").isEmpty())
+                    .andExpect(jsonPath("$.data.uuid").value(TARGET_CHATROOM_UUID))
+                    .andExpect(jsonPath("$.data.system").isNotEmpty())
+                    .andExpect(jsonPath("$.data.system.flag").value(1))
+                    .andExpect(jsonPath("$.data.system.boardId").value(TARGET_BOARD_ID));
         }
 
     }
