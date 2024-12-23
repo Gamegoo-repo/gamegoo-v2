8930425aba5d83df8d4ed3784304c8fb53eb7075
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-12-24 04:20:10 +0900

    :white_check_mark: [Test] 시스템 메시지 등록 처리 서비스 메소드 테스트

--- src/main/java/com/gamegoo/gamegoo_v2/chat/repository/ChatRepository.java
+++ src/main/java/com/gamegoo/gamegoo_v2/chat/repository/ChatRepository.java
@@ -3,6 +3,10 @@ package com.gamegoo.gamegoo_v2.chat.repository;
 import com.gamegoo.gamegoo_v2.chat.domain.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends JpaRepository<Chat, Long>, ChatRepositoryCustom {
+
+    List<Chat> findByChatroomIdAndFromMemberId(Long chatroomId, Long fromMemberId);
 
 }
--- src/test/java/com/gamegoo/gamegoo_v2/service/chat/ChatCommandServiceTest.java
+++ src/test/java/com/gamegoo/gamegoo_v2/service/chat/ChatCommandServiceTest.java
@@ -2,18 +2,25 @@ package com.gamegoo.gamegoo_v2.service.chat;
 
 import com.gamegoo.gamegoo_v2.block.domain.Block;
 import com.gamegoo.gamegoo_v2.block.repository.BlockRepository;
+import com.gamegoo.gamegoo_v2.board.domain.Board;
+import com.gamegoo.gamegoo_v2.board.repository.BoardRepository;
+import com.gamegoo.gamegoo_v2.chat.domain.Chat;
 import com.gamegoo.gamegoo_v2.chat.domain.Chatroom;
 import com.gamegoo.gamegoo_v2.chat.domain.MemberChatroom;
+import com.gamegoo.gamegoo_v2.chat.domain.SystemMessageType;
+import com.gamegoo.gamegoo_v2.chat.dto.request.SystemFlagRequest;
 import com.gamegoo.gamegoo_v2.chat.repository.ChatRepository;
 import com.gamegoo.gamegoo_v2.chat.repository.ChatroomRepository;
 import com.gamegoo.gamegoo_v2.chat.repository.MemberChatroomRepository;
 import com.gamegoo.gamegoo_v2.chat.service.ChatCommandService;
+import com.gamegoo.gamegoo_v2.config.AsyncConfig;
 import com.gamegoo.gamegoo_v2.exception.ChatException;
 import com.gamegoo.gamegoo_v2.exception.common.ErrorCode;
 import com.gamegoo.gamegoo_v2.member.domain.LoginType;
 import com.gamegoo.gamegoo_v2.member.domain.Member;
 import com.gamegoo.gamegoo_v2.member.domain.Tier;
 import com.gamegoo.gamegoo_v2.member.repository.MemberRepository;
+import com.gamegoo.gamegoo_v2.socket.SocketService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,18 +28,31 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(AsyncConfig.class)
 class ChatCommandServiceTest {
 
     @Autowired
@@ -51,7 +71,13 @@ class ChatCommandServiceTest {
     private BlockRepository blockRepository;
 
     @Autowired
+    private BoardRepository boardRepository;
+
+    @MockitoSpyBean
     private MemberRepository memberRepository;
+
+    @MockitoBean
+    private SocketService socketService;
 
     private Member member;
     private Member targetMember;
@@ -68,6 +94,7 @@ class ChatCommandServiceTest {
         memberChatroomRepository.deleteAllInBatch();
         chatroomRepository.deleteAllInBatch();
         blockRepository.deleteAllInBatch();
+        boardRepository.deleteAllInBatch();
         memberRepository.deleteAllInBatch();
     }
 
@@ -191,8 +218,163 @@ class ChatCommandServiceTest {
 
             // then
             assertThat(chatroom).isNotNull();
-            assertThat(memberChatroomRepository.findByMemberIdAndChatroomId(member.getId(), chatroom.getId())).isPresent();
-            assertThat(memberChatroomRepository.findByMemberIdAndChatroomId(targetMember.getId(), chatroom.getId())).isPresent();
+            assertThat(memberChatroomRepository.findByMemberIdAndChatroomId(member.getId(), chatroom.getId()))
+                    .isPresent();
+            assertThat(memberChatroomRepository.findByMemberIdAndChatroomId(targetMember.getId(), chatroom.getId()))
+                    .isPresent();
+        }
+
+    }
+
+    @Nested
+    @DisplayName("시스템 메시지 등록 처리")
+    class ProcessSystemMessagesTest {
+
+        @DisplayName("실패: 해당 게시글을 찾을 수 없는 경우 예외가 발생한다.")
+        @Test
+        void processSystemMessages_shouldThrownWhenBoardNotFound() {
+            // given
+            Chatroom chatroom = createChatroom();
+            createMemberChatroom(member, chatroom, null);
+            createMemberChatroom(targetMember, chatroom, null);
+
+            SystemFlagRequest request = SystemFlagRequest.builder()
+                    .boardId(10000L)
+                    .flag(1)
+                    .build();
+
+            // when // then
+            assertThatThrownBy(() -> chatCommandService.processSystemMessages(request, member, targetMember, chatroom))
+                    .isInstanceOf(ChatException.class)
+                    .hasMessage(ErrorCode.ADD_BOARD_SYSTEM_CHAT_FAILED.getMessage());
+        }
+
+        @DisplayName("실패: system member를 찾을 수 없는 경우 예외가 발생한다.")
+        @Test
+        void processSystemMessages_shouldThrownWhenSystemMemberIsNotFound() {
+            // given
+            Chatroom chatroom = createChatroom();
+            createMemberChatroom(member, chatroom, null);
+            createMemberChatroom(targetMember, chatroom, null);
+
+            Board board = createBoard(targetMember);
+
+            SystemFlagRequest request = SystemFlagRequest.builder()
+                    .boardId(board.getId())
+                    .flag(1)
+                    .build();
+
+            // when // then
+            assertThatThrownBy(() -> chatCommandService.processSystemMessages(request, member, targetMember, chatroom))
+                    .isInstanceOf(ChatException.class)
+                    .hasMessage(ErrorCode.SYSTEM_MEMBER_NOT_FOUND.getMessage());
+        }
+
+        @DisplayName("성공: 기존 lastJoinDate가 null인 경우 시스템 메시지 저장, lastJoinDate 업데이트 및 socket 이벤트가 발생해야 한다.")
+        @Test
+        void processSystemMessagesSucceedsWhenLastJoinDateIsNull() {
+            // given
+            Member systemMember = createMember("sytemMember@gmail.com", "systemMember");
+            given(memberRepository.findById(0L)).willReturn(Optional.of(systemMember));
+
+            Chatroom chatroom = createChatroom();
+            createMemberChatroom(member, chatroom, null);
+            createMemberChatroom(targetMember, chatroom, null);
+
+            Board board = createBoard(targetMember);
+
+            SystemFlagRequest request = SystemFlagRequest.builder()
+                    .boardId(board.getId())
+                    .flag(1)
+                    .build();
+
+            doNothing().when(socketService).joinSocketToChatroom(any(Long.class), any(String.class));
+
+            // when
+            chatCommandService.processSystemMessages(request, member, targetMember, chatroom);
+
+            // then
+            // 시스템 메시지 생성 되었는지 검증
+            List<Chat> systemMessages = chatRepository.findByChatroomIdAndFromMemberId(chatroom.getId(),
+                    systemMember.getId());
+            assertThat(systemMessages).hasSize(2);
+
+            Chat memberSystemMessage = systemMessages.stream()
+                    .filter(chat -> chat.getToMember().getId().equals(member.getId()))
+                    .findFirst()
+                    .orElseThrow();
+            Chat targetSystemMessage = systemMessages.stream()
+                    .filter(chat -> chat.getToMember().getId().equals(targetMember.getId()))
+                    .findFirst()
+                    .orElseThrow();
+            assertThat(memberSystemMessage.getContents()).isEqualTo(SystemMessageType.of(1).getMessage());
+            assertThat(targetSystemMessage.getContents()).isEqualTo(SystemMessageType.of(3).getMessage());
+
+            // lastJoinDate 업데이트 되었는지 검증
+            MemberChatroom memberChatroom = memberChatroomRepository
+                    .findByMemberIdAndChatroomId(member.getId(), chatroom.getId())
+                    .orElseThrow();
+            MemberChatroom targetMemberChatroom = memberChatroomRepository
+                    .findByMemberIdAndChatroomId(targetMember.getId(), chatroom.getId())
+                    .orElseThrow();
+
+            assertThat(memberChatroom.getLastJoinDate()).isNotNull();
+            assertThat(targetMemberChatroom.getLastJoinDate()).isNotNull();
+
+            // event로 인해 socketService 메소드가 호출되었는지 검증
+            await().atMost(2, TimeUnit.SECONDS).untilAsserted(() -> {
+                verify(socketService, times(2)).joinSocketToChatroom(any(Long.class), any(String.class));
+            });
+        }
+
+        @DisplayName("성공: lastJoinDate가 null이 아닌 경우 시스템 메시지만 저장되어야 한다.")
+        @Test
+        void processSystemMessagesSucceedsWhenLastJoinDateIsNotNull() {
+            // given
+            Member systemMember = createMember("sytemMember@gmail.com", "systemMember");
+            given(memberRepository.findById(0L)).willReturn(Optional.of(systemMember));
+
+            Chatroom chatroom = createChatroom();
+            LocalDateTime lastJoinDate = LocalDateTime.now().minusMinutes(10);
+            createMemberChatroom(member, chatroom, lastJoinDate);
+            createMemberChatroom(targetMember, chatroom, lastJoinDate);
+
+            Board board = createBoard(targetMember);
+
+            SystemFlagRequest request = SystemFlagRequest.builder()
+                    .boardId(board.getId())
+                    .flag(2)
+                    .build();
+
+            // when
+            chatCommandService.processSystemMessages(request, member, targetMember, chatroom);
+
+            // then
+            List<Chat> systemMessages = chatRepository.findByChatroomIdAndFromMemberId(chatroom.getId(),
+                    systemMember.getId());
+            assertThat(systemMessages).hasSize(2);
+
+            Chat memberSystemMessage = systemMessages.stream()
+                    .filter(chat -> chat.getToMember().getId().equals(member.getId()))
+                    .findFirst()
+                    .orElseThrow();
+            Chat targetSystemMessage = systemMessages.stream()
+                    .filter(chat -> chat.getToMember().getId().equals(targetMember.getId()))
+                    .findFirst()
+                    .orElseThrow();
+            assertThat(memberSystemMessage.getContents()).isEqualTo(SystemMessageType.of(2).getMessage());
+            assertThat(targetSystemMessage.getContents()).isEqualTo(SystemMessageType.of(3).getMessage());
+
+            // lastJoinDate 검증
+            MemberChatroom memberChatroom = memberChatroomRepository
+                    .findByMemberIdAndChatroomId(member.getId(), chatroom.getId())
+                    .orElseThrow();
+            MemberChatroom targetMemberChatroom = memberChatroomRepository
+                    .findByMemberIdAndChatroomId(targetMember.getId(), chatroom.getId())
+                    .orElseThrow();
+
+            assertThat(memberChatroom.getLastJoinDate()).isCloseTo(lastJoinDate, within(1, ChronoUnit.SECONDS));
+            assertThat(targetMemberChatroom.getLastJoinDate()).isCloseTo(lastJoinDate, within(1, ChronoUnit.SECONDS));
         }
 
     }
@@ -227,6 +409,20 @@ class ChatCommandServiceTest {
                 .lastJoinDate(lastJoinDate)
                 .build());
     }
+
+    private Board createBoard(Member member) {
+        return boardRepository.save(Board.builder()
+                .member(member)
+                .mode(1)
+                .mainPosition(1)
+                .subPosition(2)
+                .wantPosition(3)
+                .mike(true)
+                .content("content")
+                .boardProfileImage(1)
+                .build());
+    }
+
 
     private Block blockMember(Member member, Member targetMember) {
         return blockRepository.save(Block.create(member, targetMember));
