9cc802a9085865ce71080433de8e0fc58729c907
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-12-25 18:33:16 +0900

    :recycle: [Refactor] BaseValidator 구현 및 validator 리팩토링

--- src/main/java/com/gamegoo/gamegoo_v2/core/common/validator/ChatValidator.java
+++ src/main/java/com/gamegoo/gamegoo_v2/core/common/validator/ChatValidator.java
@@ -4,12 +4,13 @@ import com.gamegoo.gamegoo_v2.chat.domain.MemberChatroom;
 import com.gamegoo.gamegoo_v2.chat.repository.MemberChatroomRepository;
 import com.gamegoo.gamegoo_v2.core.exception.ChatException;
 import com.gamegoo.gamegoo_v2.core.exception.common.ErrorCode;
+import com.gamegoo.gamegoo_v2.core.exception.common.GlobalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class ChatValidator {
+public class ChatValidator extends BaseValidator {
 
     private final MemberChatroomRepository memberChatroomRepository;
 
@@ -22,6 +23,21 @@ public class ChatValidator {
     public MemberChatroom validateMemberChatroom(Long memberId, Long ChatroomId) {
         return memberChatroomRepository.findByMemberIdAndChatroomId(memberId, ChatroomId)
                 .orElseThrow(() -> new ChatException(ErrorCode.CHATROOM_ACCESS_DENIED));
+    }
+
+    /**
+     * 해당 채팅방을 나간 상태인 경우 입력받은 Exception을 발생시키는 메소드
+     *
+     * @param memberChatroom
+     * @param exceptionClass
+     * @param errorCode
+     * @param <T>
+     */
+    public <T extends GlobalException> void throwIfExited(MemberChatroom memberChatroom, Class<T> exceptionClass,
+                                                          ErrorCode errorCode) {
+        if (memberChatroom.exited()) {
+            throw createException(exceptionClass, errorCode);
+        }
     }
 
 }
