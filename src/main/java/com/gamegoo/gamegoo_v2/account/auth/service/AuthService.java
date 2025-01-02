566a64320f19b5e63cd224fa43150a2cecf49ff2
Rimi <lora3226@inu.ac.kr>
2025-01-03 08:27:12 +0900

    :bug: [Fix] @Transactional 어노테이션 옵션 변경

--- src/main/java/com/gamegoo/gamegoo_v2/account/auth/service/AuthService.java
+++ src/main/java/com/gamegoo/gamegoo_v2/account/auth/service/AuthService.java
@@ -5,17 +5,14 @@ import com.gamegoo.gamegoo_v2.account.auth.jwt.JwtProvider;
 import com.gamegoo.gamegoo_v2.account.auth.repository.RefreshTokenRepository;
 import com.gamegoo.gamegoo_v2.account.member.domain.Member;
 import com.gamegoo.gamegoo_v2.core.exception.JwtAuthException;
-import com.gamegoo.gamegoo_v2.core.exception.MemberException;
 import com.gamegoo.gamegoo_v2.core.exception.common.ErrorCode;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
-@Slf4j
+@Transactional(readOnly = true)
 public class AuthService {
 
     private final RefreshTokenRepository refreshTokenRepository;
@@ -26,6 +23,7 @@ public class AuthService {
      *
      * @param member 로그아웃한 회원
      */
+    @Transactional
     public void deleteRefreshToken(Member member) {
         RefreshToken refreshToken = refreshTokenRepository.findByMember(member)
                 .orElseThrow(); // 리프레시 토큰이 없을 경우 에러를 발생시킬 필요가 없음. 이미 삭제된 상태
@@ -39,9 +37,10 @@ public class AuthService {
      * @param member       로그인한 회원
      * @param refreshToken 리프레시토큰 정보
      */
+    @Transactional
     public void addRefreshToken(Member member, String refreshToken) {
         // 이전에 있던 refreshToken 전부 지우기
-        refreshTokenRepository.findByMember(member).stream().forEach(refreshTokenRepository::delete);
+        refreshTokenRepository.findByMember(member).ifPresent(refreshTokenRepository::delete);
 
         // refresh token DB에 저장하기
         refreshTokenRepository.save(RefreshToken.create(refreshToken, member));
@@ -50,7 +49,7 @@ public class AuthService {
     /**
      * 리프레시 토큰 검증
      *
-     * @param refreshToken
+     * @param refreshToken 리프레시 토큰
      */
     public void verifyRefreshToken(String refreshToken) {
         // DB 검증
