9454a72e6ba09bf519509efd3cb9ba9213cb820e
Rimi <lora3226@inu.ac.kr>
2025-01-02 18:22:31 +0900

    :bug: [Fix] git 충돌 해결

--- src/main/java/com/gamegoo/gamegoo_v2/account/auth/service/PasswordService.java
+++ src/main/java/com/gamegoo/gamegoo_v2/account/auth/service/PasswordService.java
@@ -1,6 +1,8 @@
 package com.gamegoo.gamegoo_v2.account.auth.service;
 
 import com.gamegoo.gamegoo_v2.account.member.domain.Member;
+import com.gamegoo.gamegoo_v2.core.exception.MemberException;
+import com.gamegoo.gamegoo_v2.core.exception.common.ErrorCode;
 import com.gamegoo.gamegoo_v2.utils.PasswordUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -31,6 +33,18 @@ public class PasswordService {
      */
     public boolean checkPassword(Member member, String password) {
         return PasswordUtil.matchesPassword(password, member.getPassword());
+    }
+
+    /**
+     * 비밀번호 검증
+     *
+     * @param member   회원
+     * @param password 비밀번호
+     */
+    public void verifyPassword(Member member, String password) {
+        if (!PasswordUtil.matchesPassword(password, member.getPassword())) {
+            throw new MemberException(ErrorCode.INVALID_PASSWORD);
+        }
     }
 
 }
